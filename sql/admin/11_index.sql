f33ca0358381473c3e30db62fc7a32cab7c4c40c
Yuhi-Sato <91863685+Yuhi-Sato@users.noreply.github.com>
2024-12-03 22:46:41 +0900

    index修正

--- sql/admin/11_index.sql
+++ sql/admin/11_index.sql
@@ -1,2 +1,2 @@
 ALTER TABLE visit_history DROP INDEX tenant_id_idx;
-ALTER TABLE visit_history ADD INDEX idx_tenant_id_competition_id(`tenant_id`, `competition_id`);
+ALTER TABLE visit_history ADD INDEX idx_tenant_id_competition_id_competition_id_player_id_created_at(`tenant_id`, `competition_id`, `player_id`, `created_at`);
